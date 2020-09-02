4cf56b5141861bcdf42bd208542de921a217eca4
dimasferdana <dimasferdana.janis@gmail.com>
2020-09-02 14:45:08 +0700

    Sertifikat

--- application/views/v_sertifikat.php
+++ application/views/v_sertifikat.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    <link rel="icon" href="https://diklatlpkn.id/wp-content/uploads/2017/12/cropped-logo-lpkn-new-192x192.png" sizes="192x192" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-select.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/dataTables.css') ?>">
